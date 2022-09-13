953505e6afc4dd43dfa90797b4ceec463fc3caf5
SOunit <workbookwork7@gmail.com>
2022-09-13 13:34:30 -0700

    add users

--- 02-cousera-copy/src/restaurant.route.js
+++ 02-cousera-copy/src/restaurant.route.js
@@ -12,11 +12,21 @@
         url: '/menu',
         templateUrl: 'src/public/menu/menu.html',
         controller: 'menuController',
+        resolve: {
+          users: function () {
+            return [
+              { name: 'jack', id: 1 },
+              { name: 'bec', id: 2 },
+              { name: 'kevin', id: 3 },
+              { name: 'kate', id: 4 },
+              { name: 'randal', id: 5 },
+            ];
+          },
+        },
       })
       .state('menu-items', {
         url: '/menu/{id}',
-        templateUrl: 'src/public/menu/menu.html',
-        controller: 'menuController',
+        templateUrl: 'src/public/menu-items/menu-items.html',
       });
 
     $urlRouterProvider.otherwise('/');
