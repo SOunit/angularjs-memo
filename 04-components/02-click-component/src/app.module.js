0812b5a032841f9248038cb854e61ae1b399d22a
SOunit <workbookwork7@gmail.com>
2022-09-18 16:51:53 -0700

    add memo

--- 04-components/02-click-component/src/app.module.js
+++ 04-components/02-click-component/src/app.module.js
@@ -5,12 +5,21 @@
 
   appController.$inject = ['$scope'];
   function appController($scope) {
-    $scope.users = [
+    var users = [
       { id: 1, name: 'Jack' },
       { id: 2, name: 'Bec' },
       { id: 3, name: 'Kevin' },
       { id: 4, name: 'Kate' },
       { id: 5, name: 'Randal' },
     ];
+
+    $scope.users = users;
+
+    function remove(id) {
+      console.log('appController remove: ', id);
+      // users.splice(id, 1);
+    }
+
+    $scope.remove = remove;
   }
 })();
