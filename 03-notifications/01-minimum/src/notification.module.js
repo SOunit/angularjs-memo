a7f5fee9eda04066452a90e7d59ac3a170cfcc6d
SOunit <workbookwork7@gmail.com>
2022-09-18 12:08:13 -0700

    add memo

--- 03-notifications/01-minimum/src/notification.module.js
+++ 03-notifications/01-minimum/src/notification.module.js
@@ -10,6 +10,13 @@
   notificationController.$inject = ['$scope'];
   function notificationController($scope) {
     $scope.show = false;
+    $scope.notifications = [
+      { id: 1, message: 'notification 1' },
+      { id: 2, message: 'notification 2' },
+      { id: 3, message: 'notification 3' },
+      { id: 4, message: 'notification 4' },
+      { id: 5, message: 'notification 5' },
+    ];
 
     $scope.toggleNotifications = function () {
       $scope.show = !$scope.show;
