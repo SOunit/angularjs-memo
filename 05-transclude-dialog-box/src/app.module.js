ff90b6537b338cf1cf141d1070a62634c6d7328d
SOunit <workbookwork7@gmail.com>
2022-09-18 15:19:26 -0700

    add memo

--- 05-transclude-dialog-box/src/app.module.js
+++ 05-transclude-dialog-box/src/app.module.js
@@ -6,11 +6,31 @@
   appController.$inject = ['$scope'];
   function appController($scope) {
     $scope.users = [
-      { id: 1, name: 'Jack' },
-      { id: 2, name: 'Bec' },
-      { id: 3, name: 'Kevin' },
-      { id: 4, name: 'Kate' },
-      { id: 5, name: 'Randal' },
+      {
+        id: 1,
+        name: 'Jack',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet id corporis, illum rem pariatur necessitatibus voluptate eius dolor? Illo, exercitationem. Quis fuga voluptatum at deserunt veritatis provident qui, illo ullam?',
+      },
+      {
+        id: 2,
+        name: 'Bec',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet id corporis, illum rem pariatur necessitatibus voluptate eius dolor? Illo, exercitationem. Quis fuga voluptatum at deserunt veritatis provident qui, illo ullam?',
+      },
+      {
+        id: 3,
+        name: 'Kevin',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet id corporis, illum rem pariatur necessitatibus voluptate eius dolor? Illo, exercitationem. Quis fuga voluptatum at deserunt veritatis provident qui, illo ullam?',
+      },
+      {
+        id: 4,
+        name: 'Kate',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet id corporis, illum rem pariatur necessitatibus voluptate eius dolor? Illo, exercitationem. Quis fuga voluptatum at deserunt veritatis provident qui, illo ullam?',
+      },
+      {
+        id: 5,
+        name: 'Randal',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet id corporis, illum rem pariatur necessitatibus voluptate eius dolor? Illo, exercitationem. Quis fuga voluptatum at deserunt veritatis provident qui, illo ullam?',
+      },
     ];
   }
 })();
